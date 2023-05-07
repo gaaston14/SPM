27092b8667e00141a825f41bc026a47517b76c84
Gaaston14 <gaston.ignacio.gimenez@gmail.com>
2023-05-07 14:28:55 -0300

    Se agrega codigo apra comunicarse desde vue

--- src/app.js
+++ src/app.js
@@ -10,6 +10,15 @@ const auth = require('./modulos/auth/rutas');
 
 const app = express();
 
+//CORS
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
 //Middleware
 app.use(morgan('dev'));
 app.use(express.json());
